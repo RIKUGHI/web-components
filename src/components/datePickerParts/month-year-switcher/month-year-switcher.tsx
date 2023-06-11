bf7a83ea388895b8521710275456af9eef6e2ea3
RikuGhi <rifkhairkhairur@gmail.com>
2023-06-11 16:09:37 +0700

    improve month-year-switcher

--- src/components/datePickerParts/month-year-switcher/month-year-switcher.tsx
+++ src/components/datePickerParts/month-year-switcher/month-year-switcher.tsx
@@ -1,19 +1,30 @@
-import { Component, Host, Prop, h } from '@stencil/core';
+import { Component, Prop, h } from '@stencil/core';
+import clsx from 'clsx';
 
 @Component({
   tag: 'month-year-switcher',
   styleUrl: 'month-year-switcher.css',
-  shadow: true,
 })
 export class MonthYearSwitcher {
   @Prop() label: string;
   @Prop() isNavigator: boolean;
+  @Prop() active: boolean;
+  @Prop() isYearNegative: boolean;
+  @Prop() onClick: () => void;
 
   render() {
     return (
-      <Host>
-        <button tabIndex={-1}>{this.label}</button>
-      </Host>
+      <button
+        tabIndex={-1}
+        class={clsx(
+          'w-full rounded-md py-2 text-center text-sm font-semibold font-family',
+          this.isNavigator ? (this.active ? 'bg-green-50 text-green-600' : 'hover:bg-green-50 hover:text-green-600') : this.isYearNegative ? 'text-gray-300' : 'hover:bg-gray-50',
+        )}
+        onClick={this.onClick}
+        disabled={this.isYearNegative}
+      >
+        {this.label}
+      </button>
     );
   }
 }
